34d7f1182eea42b93660a69be6eec0da6e91265b
RahamathullahK20 <42862351+RahamathullahK20@users.noreply.github.com>
2018-09-01 14:54:48 +0530

    Update greatestofthreenumbers.py

--- greatestofthreenumbers.py
+++ greatestofthreenumbers.py
@@ -2,8 +2,8 @@ a=int(raw_input("Enter the number A"))
 b=int(raw_input("Enter the number B"))
 c=int(raw_input("Enter the number C"))
 if(a>b)and(a>c):
-    print("A is Greater")
+    print(a)
 elif(b>a)and(b>c):
-    print("B is Greater")
+    print(b)
 else:
-    print("C is Greater")
+    print(c)
