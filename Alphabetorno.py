98bb312808b293671c26ca19a8cf0f1b88ec73c7
RahamathullahK20 <42862351+RahamathullahK20@users.noreply.github.com>
2019-02-08 12:25:50 +0530

    Update Alphabetorno.py

--- Alphabetorno.py
+++ Alphabetorno.py
@@ -1,4 +1,4 @@
-x=str(raw_input("Enter the letter"))
+x=str(input())
 if x in('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','x','z','a','e','i','o','u'):
     print("Alphabet")
 else:
