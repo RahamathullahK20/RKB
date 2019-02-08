e2d718bce8612a3ea2941e97a3e3e8d56a375195
RahamathullahK20 <42862351+RahamathullahK20@users.noreply.github.com>
2019-02-08 13:47:59 +0530

    Update PorN.py

--- PorN.py
+++ PorN.py
@@ -1,11 +1,7 @@
-def main():
-    x=int(raw_input("Enter the number"))
-    if(x>0):
+x=int(input())
+if(x>0):
 	print("positive")
-    elif(x<0):
+elif(x<0):
 	print("Negative")
-    else:
+else:
 	print("Zero")
-
-if __name__ == '__main__':
-    main()
